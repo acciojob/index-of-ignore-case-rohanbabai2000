490d107f4d30cedf11434c8414fcfdaeb71e55b6
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-12-24 23:42:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	const cleanStr = [];
-    const indexMap = [];
-
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] !== " ") {
-            cleanStr.push(str[i].toLowerCase());
-            indexMap.push(i); // map cleaned index to original index
-        }
-    }
-
-    const cleanSub = subStr.replace(/\s+/g, "").toLowerCase();
-
-    // Manual substring search
-    for (let i = 0; i <= cleanStr.length - cleanSub.length; i++) {
-        let match = true;
-
-        for (let j = 0; j < cleanSub.length; j++) {
-            if (cleanStr[i + j] !== cleanSub[j]) {
-                match = false;
-                break;
-            }
-        }
-
-        if (match) {
-            return indexMap[i]; // return original index
-        }
-    }
-
-    return -1;
+	return str.toLowerCase().indexOf(subStr.toLowerCase());
 }
 
 // Please do not change the code below
